831222867a68033989367e5bc8825a3c3fdfe322
David De Castro <segphault@gmail.com>
2023-08-13 17:53:34 +0200

    On continue sur la shopping list avec l'ajout de conditions et de styles

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from "../datas/plantList";
+import '../styles/ShoppingList.css';
 
 function ShoppingList() {
     const categories = plantList.reduce(
@@ -13,11 +14,11 @@ function ShoppingList() {
                     <li key={cat}>{cat}</li>
                 ))}
             </ul>
-            <ul>
+            <ul className="lmj-plant-list">
                 {plantList.map( (plant) => (
-                    <li key={plant.id}>
-                        {plant.name}
-                        {plant.isBestSale && <span>🔥</span>}
+                    <li key={plant.id} className="lmj-plant-item">
+                        {plant.name} {plant.isBestSale && <span>🔥</span>}
+                        {plant.isSpecialOffer && <div className="lmj-sales">En promo</div>}
                     </li>
                 ))}
             </ul>
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -3,54 +3,63 @@ export const plantList = [
 		name: 'monstera',
 		category: 'classique',
 		id: '1ed',
-        isBestSale: true
+        isBestSale: true,
+        isSpecialOffer: false
 	},
 	{
 		name: 'ficus lyrata',
 		category: 'classique',
 		id: '2ab',
-        isBestSale: false
+        isBestSale: false,
+        isSpecialOffer: false
 	},
 	{
 		name: 'pothos argenté',
 		category: 'classique',
 		id: '3sd',
-        isBestSale: true
+        isBestSale: true,
+        isSpecialOffer: true
 	},
 	{
 		name: 'yucca',
 		category: 'classique',
 		id: '4kk',
-        isBestSale: false
+        isBestSale: false,
+        isSpecialOffer: false
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-        isBestSale: true
+        isBestSale: true,
+        isSpecialOffer: false
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
 		id: '6uo',
-        isBestSale: false
+        isBestSale: false,
+        isSpecialOffer: false
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-        isBestSale: false
+        isBestSale: false,
+        isSpecialOffer: true
 	},
 	{
 		name: 'aloe',
 		category: 'plante grasse',
 		id: '8fp',
-        isBestSale: true
+        isBestSale: true,
+        isSpecialOffer: true
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
 		id: '9vn',
-        isBestSale: false
+        isBestSale: false,
+        isSpecialOffer: false
 	}
 ];
