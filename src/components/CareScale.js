03d0727f428b705c7bd2fda993ee34fe719746a7
David De Castro <segphault@gmail.com>
2023-08-15 14:30:03 +0200

    Exercice 1 à ma manière

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -6,6 +6,18 @@ const quantityLabel = {
     2: 'modérément',
     3: 'beaucoup'
 }
+
+function handleClick(careType, scaleValue) {
+    const quantityText = quantityLabel[scaleValue];
+    let text='';
+    if (careType==='light') {
+        text='Cette plante requiert '+quantityText+' de soleil';
+    } else {
+        text='Cette plante requiert '+quantityText+" d'eau";
+    }
+
+    alert(text);
+}
 // On peut passer les paramètres aussi de cette manière
 // function CareScale({scaleValue, careType}) {
 function CareScale(props) {
@@ -21,16 +33,7 @@ function CareScale(props) {
                 <img src={Water} alt='water-icon' />
             );
 
-    return (<div
-                onClick={ () =>
-                    alert(
-                        `Cette plante requiert ${quantityLabel[scaleValue]} ${
-                                careType === 'light' ? 'de lumière' : "d'arrosage"
-                        }`
-                    )
-
-                }
-            >
+    return (<div onClick={ () => handleClick(careType,scaleValue)}>
                 {range.map((rangeElem) =>
                     scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
                 )}
